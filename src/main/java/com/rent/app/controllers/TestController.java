8e9ee0256856c5a58c8e72c52d9a204cf003a6f4
DMC\kumarmah <mahesh.kumarmurugesan@diconium.com>
2022-10-20 15:19:09 +0200

    user

--- src/main/java/com/rent/app/controllers/TestController.java
+++ src/main/java/com/rent/app/controllers/TestController.java
@@ -1,15 +1,26 @@
 package com.rent.app.controllers;
 
+import com.rent.app.models.User;
+import com.rent.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+  @Autowired
+  UserRepository userRepository;
+
+
   @GetMapping("/all")
   public String allAccess() {
     return "Public Content.";
@@ -31,5 +42,11 @@ public class TestController {
   @PreAuthorize("hasRole('ADMIN')")
   public String adminAccess() {
     return "Admin Board.";
+  }
+
+  @GetMapping("/users")
+//  @PreAuthorize("hasRole('ADMIN')")
+  public List<User> getUsers() {
+    return userRepository.findAll();
   }
 }
